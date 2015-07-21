33e37659ea8c791a1130af0c1b30076d84cffad1
dean.aldridge.nz@gmail.com <dean.aldridge.nz@gmail.com>
2015-07-22 01:11:04 +1200

    setting up grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -162,7 +162,7 @@ module.exports = function (grunt) {
           sourceMapURL: '<%= pkg.name %>.css.map',
           sourceMapFilename: 'dist/css/<%= pkg.name %>.css.map'
         },
-        src: 'less/bootstrap.less',
+        src: 'less/app.less',
         dest: 'dist/css/<%= pkg.name %>.css'
       },
       compileTheme: {
@@ -528,4 +528,6 @@ module.exports = function (grunt) {
       done();
     });
   });
+  //grunt.loadNpmTasks('grunt-livereload');grunt dist
+
 };
