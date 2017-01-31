0892d8ee3326ac636361bcf147f18f2ff29aa9fa
dean.aldridge.nz@gmail.com <dean.aldridge.nz@gmail.com>
2017-01-31 13:53:07 +1300

    January 2017 update - push menu

--- scripts/mbs.js
+++ scripts/mbs.js
@@ -1,40 +1,9 @@
-$(function() {
-	
-	var scroller  = $(".scroller-content");
-	var bodymove = $("html, body");
-	var controls = $("#glider .controls");
-	
-	var slideScreen = function (e) {
-	
-		var screenName = location.hash + "-screen";
-		var itemToView = $(screenName);
-		var leftPostion = itemToView.position().left * -1;
-		scroller.animate({left: leftPostion}, "slow");
-		bodymove.animate({ scrollTop: 0 }, "slow");
-		 
-		 controls.find('.selected').removeClass('selected');
-		 controls.find('[href="' + location.hash + '"]').addClass('selected');
-
-	};
-	
-	var gotoScreen = function(screenName) {
-		if (!screenName) {
-			return; // no screen name, no animating
-		}
-		
-		var itemToView = $(screenName + "-screen");
-		var leftPosition = itemToView.position().left * -1;
-		scroller.css({ left: leftPosition });
-		
-		 controls.find('.selected').removeClass('selected');
-		 controls.find('[href="' + screenName + '"]').addClass('selected');
-		 
-	};
-
-	$(window).hashchange(slideScreen);
-	gotoScreen(location.hash);
-
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > 180) {
+        $('.controls').addClass('fixed');
+        $("back-to-top").css('display:block;');
+    } else {
+        $('.controls').removeClass('fixed');
+        $("back-to-top").css('display:none;');
+    }
 });
-
-
-// TODO: write a function to modularise d/selecting current page
